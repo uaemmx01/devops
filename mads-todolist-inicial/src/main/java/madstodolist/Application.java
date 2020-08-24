f8561d24afc8dad5bed13771c19f7008ed330f06
root <root@ip-172-31-71-142.ec2.internal>
2020-08-24 23:18:16 +0000

    again

--- mads-todolist-inicial/src/main/java/madstodolist/Application.java
+++ mads-todolist-inicial/src/main/java/madstodolist/Application.java
@@ -2,7 +2,7 @@ package madstodolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
